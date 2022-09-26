1484a43da664e605464b8ee098678d998c16568d
Marcio <semEmail@gmafil.com>
2022-09-26 12:45:41 -0300

    results based on criteria(pagination)

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { list } = require("./controller/listController");
 
 router.route("/").get((req, res) => res.json("Main -> /"));
+
 router.route("/test").get((req, res) => res.json("Test -> /test"));
+
+router.route("/list/:page/:result_amount").get(list);
 
 module.exports = router;
