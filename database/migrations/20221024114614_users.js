dc893b9ad1d890a8860caaad44d9b78408f5cf8d
Marcio <marciodgart@gmail.com>
2022-10-24 10:00:19 -0300

    created users migration

---
+++ database/migrations/20221024114614_users.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("email").unique().notNullable();
+    table.text("password").notNullable();
+    table.specificType("watchlist", "text ARRAY");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
